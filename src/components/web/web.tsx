cdc58a5ebc6d1c611afe4c7222d0f5ca7d1dd469
alessandrobarreto <alessandrobarreto1201@gmail.com>
2021-12-03 11:49:09 -0300

    feature: header web component

--- src/components/web/web.tsx
+++ src/components/web/web.tsx
@@ -5,27 +5,27 @@ export default function Web() {
     <div className="web">
       <div className="web-option">
         <a href="#project">
-          <i className="fi fi-rr-edit-alt menu-icon"></i>
+          <i className="fi fi-rr-edit-alt option-icon"></i>
+          Projects
         </a>
-        Projects
       </div>
       <div className="web-option">
         <a href="#skills">
           <i className="fi fi-rr-laptop option-icon"></i>
+          Skills
         </a>
-        Skills
       </div>
       <div className="web-option">
         <a href="#work">
           <i className="fi fi-rr-briefcase option-icon"></i>
+          Work
         </a>
-        Work
       </div>
       <div className="web-option">
         <a href="">
-          <i className="fi fi-rr-user"></i>
+          <i className="fi fi-rr-user option-icon"></i>
+          Contact
         </a>
-        Contact me
       </div>
     </div>
   );
