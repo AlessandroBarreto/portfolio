d61642afa01e4e3fa1f34633b113bbcdefeba001
alessandrobarreto <alessandrobarreto1201@gmail.com>
2021-12-03 12:38:40 -0300

    feature: header mobile component

--- src/components/mobile/mobile.tsx
+++ src/components/mobile/mobile.tsx
@@ -1,12 +1,42 @@
-interface props{
-    isOpen: boolean
-    setIsOpen: Function
+import "../../styles/mobile.scss";
+
+interface mobileProps {
+  isOpen: boolean;
+  setIsOpen: Function;
 }
 
-export default function Mobile({isOpen, setIsOpen} : props) {
-    return (
-        <div>
-            mobile
+export default function Mobile({ isOpen, setIsOpen }: mobileProps) {
+  return (
+    <div className="mobile">
+      <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
+        <i className="fi fi-rr-cross-circle"></i>
+      </div>
+      <div className="mobile-options">
+        <div className="mobile-option">
+          <a href="#project">
+            <i className="fi fi-rr-edit-alt option-icon"></i>
+            Projects
+          </a>
         </div>
-    )
+        <div className="mobile-option">
+          <a href="#skills">
+            <i className="fi fi-rr-laptop option-icon"></i>
+            Skills
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#work">
+            <i className="fi fi-rr-briefcase option-icon"></i>
+            Work
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="">
+            <i className="fi fi-rr-user option-icon"></i>
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
