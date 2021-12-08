9ded15a4b39f52fb00560a24fb5157d9596f2b61
alessandrobarreto <alessandrobarreto1201@gmail.com>
2021-12-08 16:54:13 -0300

    feature: separator and projects components

--- src/components/body/projects.tsx
+++ src/components/body/projects.tsx
@@ -1,10 +1,12 @@
 import ProjectCard from "../common/projectCard";
+import Separator from "../common/separator";
 import { ProjectData } from "../../data/projects";
 import "../../styles/projects.scss";
 
 export default function Projects() {
   return (
     <div className="projects">
+      <Separator/>
       <label>Projects</label>
       <div>
         {ProjectData.map((project) => {
---
+++ src/components/common/separator.tsx
@@ -0,0 +1,5 @@
+import "../../styles/separator.scss";
+
+export default function Separator() {
+  return <div className="separator"></div>;
+}
